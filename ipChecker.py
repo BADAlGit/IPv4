f7bd6c65fa9a58614cce627037c6c8ab54aba0c5
Badal <bad07al@gmail.com>
2022-07-05 00:20:43 +0530

    Finally save the tuples of list into conversion.txt and display the required output.

--- ipChecker.py
+++ ipChecker.py
@@ -1,4 +1,7 @@
-def validIPAddress( IP):
+import pickle
+
+
+def validIPAddress(IP):
 
     blocks = IP.split('.')
     if len(blocks) == 4:
@@ -7,24 +10,37 @@ def validIPAddress( IP):
                (blocks[i][0] == '0' and len(blocks[i]) > 1):
                 return False
         return True
-        
+
 
 storeIp = []
-
-for i in range(2):
+n = int(input("Enter total number of IPv4 adresses: "))
+for i in range(n):
     IPv4 = input("Enter IPv4: ")
-    if(validIPAddress(IPv4)==True):
-        binaryValue = '.'.join([bin(int(x)+256)[3:] for x in IPv4.split('.')]) 
-        octalValue ='.'.join(["%04o" % int(x) for x in IPv4.split('.')])
+    if(validIPAddress(IPv4) == True):
+        binaryValue = '.'.join([bin(int(x)+256)[3:] for x in IPv4.split('.')])
+        octalValue = '.'.join(["%04o" % int(x) for x in IPv4.split('.')])
         hexaValue = '.'.join([hex(int(i))[2:] for i in IPv4.split('.')])
-        storeIp.append(IPv4, binaryValue, octalValue, hexaValue))
+        storeIp.append((IPv4, binaryValue, octalValue, hexaValue))
         continue
     else:
         print("It is not a validIPAddress")
         # break
- 
-for i in range(len(storeIp)): 
-    print(storeIp[i])
+# for i in storeIp:
+#     print(i)
 
-# 256.256.256.256 : Not a valid Ipv4 address
-# 172.16.254.1 : valid Ipv4 address
+f = open('conversion.txt', 'w')
+for t in storeIp:
+    line = ', '.join(str(x) for x in t)
+    f.write(line + '\n')
+f.close()
+
+IPFile = open('conversion.txt', 'r')
+# Lines = file1.readlines()
+count = 0
+for line in IPFile:
+    count += 1
+    print("The {} adreess om Decimal, Binary, Octal and Hexadecimal format is : {}".format(
+        count, line.strip()))
+
+# Closing files
+IPFile.close()
